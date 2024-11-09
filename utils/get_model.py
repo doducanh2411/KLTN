5ffbdacfe4de9f3b2d8881fb8e3f6499a21341ff
Đức Anh <anh.dd@teko.vn>
2024-11-09 16:38:20 +0700

    feat: freeze bert

--- utils/get_model.py
+++ utils/get_model.py
@@ -6,7 +6,7 @@ def get_model(model_name, num_classes, num_frames=None):
     if model_name == 'single_frame':
         return SingleFrame(num_classes)
     elif model_name == 'early_fusion':
-        return EarlyFusion(num_classes)
+        return EarlyFusion(num_classes, num_input_channels=num_frames)
     elif model_name == 'late_fusion':
         return LateFusion(num_classes)
     elif model_name == 'cnn_lstm':
